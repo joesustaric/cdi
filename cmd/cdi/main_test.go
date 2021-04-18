5a462892409b5f188fdc2b5c0c71472c485295d4
Joe Sustaric <joe8307@gmail.com>
2021-04-18 20:57:38 +1000

    added a test

---
+++ cmd/cdi/main_test.go
@@ -0,0 +1,20 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TODO: Figure out how to test main finction
+// func TestMain(m *testing.M) {
+// 	exitCode := m.Run()
+
+// 	os.Exit(exitCode)
+// }
+
+func TestSomething(t *testing.T) {
+
+	assert.True(t, true, "True is true!")
+
+}
