8b1de32b0c502d77d10e5da13e11236cd9a6ef11
Joe Sustaric <joe8307@gmail.com>
2021-06-06 13:37:46 +1000

    adding in a github graphql client class we can use

--- cmd/cdi/clients/github.go
+++ cmd/cdi/clients/github.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/shurcooL/githubv4"
@@ -14,19 +15,38 @@ type GitHubClient struct {
 	gitHubClient *githubv4.Client
 }
 
-func NewGitHubClient() (*GitHubClient, error) {
+func NewGitHubClient(httpClient *http.Client) (*GitHubClient, error) {
 	tokenVal, ok := os.LookupEnv("GITHUB_TOKEN")
 	if !ok {
 		log.Printf("%s not set\n", "GITHUB_TOKEN")
 		return nil, errors.New("GITHUB_TOKEN not set")
 	}
 
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: tokenVal},
-	)
-	httpClient := oauth2.NewClient(context.Background(), src)
+	if httpClient == nil {
+		src := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: tokenVal},
+		)
+		httpClient = oauth2.NewClient(context.Background(), src)
+	}
+
 	client := githubv4.NewClient(httpClient)
 
 	return &GitHubClient{gitHubClient: client}, nil
 }
 
+func (c *GitHubClient) RawBranchCount(repo string) (int, error) {
+	var query struct {
+		Repository struct {
+			Id   string
+			Refs struct {
+				TotalCount int
+			} `graphql:"refs(refPrefix: \"refs/heads/\")"`
+		} `graphql:"repository(owner: \"joesustaric\", name: \"cdi-test-repo\")"`
+	}
+
+	err := c.gitHubClient.Query(context.Background(), &query, nil)
+	if err != nil {
+		return 0, err
+	}
+	return query.Repository.Refs.TotalCount, nil
+}
--- cmd/cdi/clients/github_test.go
+++ cmd/cdi/clients/github_test.go
@@ -1,6 +1,10 @@
 package clients_test
 
 import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
@@ -9,20 +13,68 @@ import (
 )
 
 func TestNewGitHubClient_Error_When_No_Token(t *testing.T) {
-
-	_, e := clients.NewGitHubClient()
+	_, e := clients.NewGitHubClient(nil)
 
 	assert.NotNil(t, e)
 }
 
 func TestNewGitHubClient_No_Error_With_Token(t *testing.T) {
 	os.Setenv("GITHUB_TOKEN", "123456")
+	defer os.Unsetenv("GITHUB_TOKEN")
 
-	client, e := clients.NewGitHubClient()
+	client, e := clients.NewGitHubClient(nil)
 
 	assert.Nil(t, e)
 	assert.NotNil(t, client)
-
-	os.Unsetenv("GITHUB_TOKEN")
 }
 
+func TestClient_Returns_Raw_Branches(t *testing.T) {
+	os.Setenv("GITHUB_TOKEN", "123456")
+	defer os.Unsetenv("GITHUB_TOKEN")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/graphql", func(w http.ResponseWriter, req *http.Request) {
+
+		got := mustRead(req.Body)
+		want := `{"query":"{repository(owner: \"joesustaric\", name: \"cdi-test-repo\"){id,refs(refPrefix: \"refs/heads/\"){totalCount}}}"}` + "\n"
+		assert.Equal(t, got, want)
+
+		w.Header().Set("Content-Type", "application/json")
+		resp := `{"data": {"repository": {"id": "someid","refs": {"totalCount": 23}}}}`
+		mustWrite(w, resp)
+	})
+	testClient := &http.Client{Transport: localRoundTripper{handler: mux}}
+
+	githubClient, _ := clients.NewGitHubClient(testClient)
+
+	repo := "https://github.com/joesustaric/cdi-test-repo.git"
+
+	branches, _ := githubClient.RawBranchCount(repo)
+
+	assert.Equal(t, 23, branches)
+}
+
+type localRoundTripper struct {
+	handler http.Handler
+}
+
+func (l localRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	w := httptest.NewRecorder()
+	l.handler.ServeHTTP(w, req)
+	return w.Result(), nil
+}
+
+func mustRead(r io.Reader) string {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
+func mustWrite(w io.Writer, s string) {
+	_, err := io.WriteString(w, s)
+	if err != nil {
+		panic(err)
+	}
+}
